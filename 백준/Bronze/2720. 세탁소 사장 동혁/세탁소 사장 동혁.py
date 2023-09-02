5fc46c3c385d1cf98fc0ad1f34590620cc105b24
DG Kim <143414166+gyun97@users.noreply.github.com>
2023-09-02 21:07:44 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,12 +1,9 @@
 T = int(input())
+changes = [25, 10, 5, 1]
 
 for _ in range(T):
     C = int(input())
-    count_quarter = C//25
-    C = C%25
-    count_dime = C//10
-    C = C%10
-    count_nickel = C//5
-    C = C%5
-    count_penny = C//1
-    print(count_quarter, count_dime, count_nickel, count_penny, sep=' ')
+    for change in changes:
+        print(C//change, end=" ")
+        C = C%change
+    print("")
