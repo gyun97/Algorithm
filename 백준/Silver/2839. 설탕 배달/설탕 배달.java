eec007dc1837fc9936e412fec3e199fdb4285784
K.D.G <143414166+gyun97@users.noreply.github.com>
2023-09-19 12:46:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (true) {
+            if (N % 5 == 0) {
+                count += N / 5;
+                sb.append(count);
+                break;
+            } else {
+                N -= 3;
+                count += 1;
+                if (N < 0) {
+                    sb.append(-1);
+                    break;
+                }
+
+            }
+        }
+        System.out.println(sb);
+    }
+}
