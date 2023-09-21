3614c18ddaa6af5623c71d652663b1bbf867e732
K.D.G <143414166+gyun97@users.noreply.github.com>
2023-09-21 14:17:49 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,21 +1,17 @@
 import sys
 
-square = [list(map(int, sys.stdin.readline().split())) for _ in range(3)]
+list_x = []
+list_y = []
 
-if square[0][0] == square[1][0]:
-    last_x = square[2][0]
-elif square[1][0] == square[2][0]:
-    last_x = square[0][0]
-else:
-    last_x = square[1][0]
+for _ in range(3):
+    x, y = map(int, sys.stdin.readline().split())
+    list_x.append(x)
+    list_y.append(y)
 
-if square[0][1] == square[1][1]:
-    last_y = square[2][1]
-elif square[1][1] == square[2][1]:
-    last_y = square[0][1]
-else:
-    last_y = square[1][1]
+for x, y in zip(list_x, list_y):
+    if list_x.count(x) == 1:
+        last_x = x
+    if list_y.count(y) == 1:
+        last_y = y
 
-square.append([last_x, last_y])
-
-print(*square[3])
+print(last_x, last_y)
