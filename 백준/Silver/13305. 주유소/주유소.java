99be2e2c304b8fb48c57a997bbd8fb35dec9d8c4
K.D.G <143414166+gyun97@users.noreply.github.com>
2023-12-01 23:59:18 +0900

    [Silver III] Title: 주유소, Time: 332 ms, Memory: 30684 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dist = new int[N - 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N - 1; i++) {
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] cost = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int j = 0; j < N; j++) {
+            cost[j] = Integer.parseInt(st.nextToken());
+        }
+
+        int min_cost = cost[0];
+        int total_cost = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            if (cost[i] < min_cost) {
+                min_cost = cost[i];
+            }
+            total_cost += min_cost * dist[i];
+
+        }
+
+        sb.append(total_cost);
+        System.out.println(sb);
+
+
+    }
+}
+
+
+
+
+
