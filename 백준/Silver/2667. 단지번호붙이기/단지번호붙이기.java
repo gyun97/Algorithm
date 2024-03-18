cc676ed0d0a9dc7a67bc737ea0404c18eb571ee9
K.D.G <143414166+gyun97@users.noreply.github.com>
2024-03-18 15:24:38 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -6,11 +6,10 @@ import java.util.*;
 
 class Main {
 
-    static int N;   // 지도의 크기: N * N
-    static int[][] graph;   // 지도
-    static int[] dx = {1, -1, 0, 0};  // 상하
-    static int[] dy = {0, 0, 1, -1};  // 좌우
-    static int count;   // 단지 내 집의 수
+    static int N, count;
+    static int[][] map;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
 
     public static void main(String[] args) throws IOException {
@@ -19,54 +18,54 @@ class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        graph = new int[N][N];
+        map = new int[N][N];
 
 
-        ArrayList<Integer> result = new ArrayList<>();   // 단지 내 집의 수를 저장할 리스트
-
-        // 지도 입력
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < N; j++) {
                 char ch = str.charAt(j);
-                graph[i][j] = ch - '0';
-
+                map[i][j] = ch - '0';
             }
         }
 
-        // 지도 모든 인덱스 돌면서 1이면 dfs 수행하고 결과를 result에 저장
+        ArrayList<Integer> result = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 count = 0;
-                if (graph[i][j] == 1) {
+                if (map[i][j] == 1) {
                     dfs(i, j);
                     result.add(count);
 
                 }
+
             }
         }
 
         Collections.sort(result);
-        
+        System.out.println(result.size());
+//        for (int i = 0; i < result.size(); i++) {
+//            System.out.println(result.get(i));
+//        }
+        result.forEach(System.out::println);
+//        result.stream().forEach(System.out::println);
 
-        System.out.println(result.size());  // 단지 수 출력
-        for (int i = 0; i < result.size(); i++) {   // 단지 내 집의 수 출력
-            System.out.println(result.get(i));
-        }
     }
 
-    public static void dfs(int x, int y) {   // dfs 수행
-        graph[x][y] = 0;    // 방문했다고 표시하고
-        count++;    // 단지 내 집의 수를 1 증가시키고
-
-
+    private static void dfs(int x, int y) {
+        map[x][y] = 0;
+        count++;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];     // 상하좌우로 이동하면서
+            int nx = x + dx[i];
             int ny = y + dy[i];
-            if ( (0 <= nx && nx < N) && (0 <= ny && ny < N) && (graph[nx][ny] == 1)) {   // 지도를 벗어나지 않고, 아직 방문하지 않은 집이 있으면
-                dfs(nx, ny);    // 재귀적으로 dfs 수행
+            if (0 <= nx && nx < N && 0 <= ny && ny < N && map[nx][ny] == 1) {
+                dfs(nx, ny);
             }
         }
 
+
     }
+
+
 }
