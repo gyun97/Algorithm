986523e51b8c5bf451c8602ca2258509807ac0ca
K.D.G <143414166+gyun97@users.noreply.github.com>
2024-04-08 21:39:40 +0900

    [Silver III] Title: 블로그2, Time: 280 ms, Memory: 22576 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.java
+++ 백준/Silver/20365. 블로그2/블로그2.java
@@ -18,16 +18,16 @@ class Main {
         colorCount.put('B', 0);
         colorCount.put('R', 0);
 
-        colorCount.replace(input.charAt(0), 1);
+        colorCount.replace(input.charAt(0), 1);  // 첫번째 색깔은 무조건 칠해야함
 
-        for (int i = 1; i < N; i++) {
-            char current = input.charAt(i);
-            if (current != input.charAt(i - 1)) {
-                colorCount.replace(current, colorCount.get(current) + 1);
+        for (int i = 1; i < N; i++) {  // 현재 색깔과 이전 색깔이 다르면 칠해야함
+            char current = input.charAt(i);  // 현재 색깔
+            if (current != input.charAt(i - 1)) {  // 이전 색깔과 다르면 칠해야함
+                colorCount.replace(current, colorCount.get(current) + 1);  // 칠함
             }
         }
 
-        int answer = Math.min(colorCount.get('B'), colorCount.get('R')) + 1;
+        int answer = Math.min(colorCount.get('B'), colorCount.get('R')) + 1;    // 더 작은 색깔의 수 + 1(더 많은 색깔로 전체 칠함
         sb.append(answer);
         System.out.println(answer);
 
