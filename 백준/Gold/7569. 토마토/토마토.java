b99d862f4e0ca793cba2b6fc0dd9c1446dc29a19
KDG <143414166+gyun97@users.noreply.github.com>
2024-06-20 22:21:27 +0900

    [Gold V] Title: 토마토, Time: 588 ms, Memory: 121892 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -7,111 +7,100 @@ import java.util.StringTokenizer;
 
 
 
-
-class Point3D {
-    int z, x, y, count;
+class Point3D{  // 3차원 좌표 클래스
+    int z, x, y;
 
     public Point3D(int z, int x, int y) {
-        this.z = z;
-        this.x = x;
-        this.y = y;
-
+        this.z = z;  // H 대응값. 상자 층 수
+        this.x = x;  // N 대응값. 상자 세로
+        this.y = y;  // M 대응값. 상자 가로
     }
-
 }
 
 
-class Main {
+public class Main {
 
-    static int N, M, H;
-    static int[][][] tomatoes;
+    static StringBuilder sb = new StringBuilder(); // StringBuilder에 출력값 저장
+
+    static int M, N, H;
+    static int[][][] tomato;  // 토마토 상자(3차원 배열)
+
     static Queue<Point3D> queue = new LinkedList<>();
-    static int day = 0;
+    static int[] dz = {1, -1, 0, 0, 0, 0};  // 상자 1층씩  위아래 이동
+    static int[] dx = {0, 0, 1, -1, 0, 0};  // 상자 칸 상하(세로) 한 칸씩 이동
+    static int[] dy = {0, 0, 0, 0, 1, -1};  // 상자 칸 좌우(가로) 한 칸씩 이동
 
-    static StringBuilder sb = new StringBuilder();
+    static int day;  // 각 칸의 토마토가 익는 데 걸리는 최소 날짜(에 + 1 되어 있음)
+
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] input = br.readLine().split(" ");
+        M = Integer.parseInt(input[0]);  // 가로
+        N = Integer.parseInt(input[1]);  // 세로
+        H = Integer.parseInt(input[2]);  // 위아래
+        tomato = new int[H][N][M];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        M = Integer.parseInt(st.nextToken()); // 상자의 가로 칸 수
-        N = Integer.parseInt(st.nextToken()); // 상자의 세로 칸 수
-        H = Integer.parseInt(st.nextToken()); // 쌓아올려진 상자 수
-
-        tomatoes = new int[H][N][M];
-
-
+        // 입력값 받아 3차원 배열 생성
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < N; j++) {
-                st = new StringTokenizer(br.readLine(), " ");
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
                 for (int k = 0; k < M; k++) {
-                    tomatoes[i][j][k] = Integer.parseInt(st.nextToken());
-                    if (tomatoes[i][j][k] == 1) {
-                        queue.add(new Point3D(i, j, k));
-                    }
+                    tomato[i][j][k] = Integer.parseInt(st.nextToken());
+                    if (tomato[i][j][k] == 1) queue.add(new Point3D(i, j, k));  // 이미 익은 토마토들 위치 큐에 추가
                 }
-            }
+                }
         }
-        bfs();
 
-        checkingResult();
+        bfs(); // BFS 시작
+        checkResult();
 
+
+        // 첫 날에 값이 1인 토마토(익은 토마토)에서 시작했는데 첫 날은 day = 0이기 때문에 최소 날짜보다 1 더 잡힘
+        // (각 토마토의 값은 익는데 걸린 최소 날짜)
         sb.append(day - 1);
-        System.out.println(sb);
+        System.out.println(sb); // 출력
+
 
     }
 
-    private static void checkingResult() {
-
-            LOOP:
-            for (int i = 0; i < H; i++) {
-                for (int j = 0; j < N; j++) {
-                    for (int k = 0; k < M; k++) {
-                        if (tomatoes[i][j][k] == 0) {
-                            sb.append(-1);
-                            System.out.println(sb);
-                            System.exit(0);
-                        } else {
-                            day = Math.max(tomatoes[i][j][k], day);
-                        }
+    // 토마토가 모두 익지 못하는 상황 조건 처리 메서드
+    private static void checkResult() {
+        //        LOOP:
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (tomato[i][j][k] == 0) {
+                        sb.append(-1);
+                        System.out.println(sb);
+                        System.exit(0);  // 바로 프로그램(JVM) 정상 종료 처리
+//                        break LOOP; -> TIP: 반복문 여러 개 겹쳐 있으면 가장 바깥 반복문에 라벨링하면 break 조건으로 중첩 반복문 바로 탈출 가능
+                    } else {
+                        day = Math.max(day, tomato[i][j][k]);  // 토마토가 모두 익을 때까지의 최소 날짜니까 비교하면서 익는 데 가장 오래 걸린 토마토 찾아야 한다.
                     }
                 }
             }
 
         }
+    }
 
-
+    // BFS 메서드
     private static void bfs() {
-        while (!queue.isEmpty()) {
-            Point3D currentPoint = queue.poll();
-            int[] dz = {0, 0, 0, 0, 1, -1};
-            int[] dx = {1, -1, 0, 0, 0, 0};
-            int[] dy = {0, 0, 1, -1, 0, 0};
-
+        while (!queue.isEmpty()) {  // 큐가 빌 때까지
+            Point3D current = queue.poll();  // 큐에서 토마토 꺼내기
             for (int i = 0; i < 6; i++) {
-                int nz = currentPoint.z + dz[i];
-                int nx = currentPoint.x + dx[i];
-                int ny = currentPoint.y + dy[i];
-
-                if (0 > nz || nz >= H) continue;
-                if (0 > nx || nx >= N) continue;
-                if (0 > ny || ny >= M) continue;
-                if (tomatoes[nz][nx][ny] == 0) {
-                    queue.add(new Point3D(nz, nx, ny));
-                    tomatoes[nz][nx][ny] = tomatoes[currentPoint.z][currentPoint.x][currentPoint.y] + 1;
+                int nz = current.z + dz[i];  // 위아래(층)로 이동
+                int nx = current.x + dx[i];  // 상하로 이동
+                int ny = current.y + dy[i];  // 좌우로 이동
+                if (0 > nz || nz >= H || 0 > nx || nx >= N || 0 > ny || ny >= M) continue;  // 범위 밖이면 무시
+                if (tomato[nz][nx][ny] == 0) {  // 아직 안 익은 토마토면
+                    tomato[nz][nx][ny] = tomato[current.z][current.x][current.y] + 1; // 익는 데 영향 준 인접한 토마토보다 하루 더 걸림
+                    queue.add(new Point3D(nz, nx, ny));  // 주변 다른 토마토들에게도 연쇄 영향 주기 위해 큐에 추가 
                 }
-
             }
-
         }
-
-
     }
 
 
-    }
-
-
+}
