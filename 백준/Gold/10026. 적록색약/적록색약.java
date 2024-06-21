c8f2d1149ed4b88af6fea7784a9c4a1cf958ba01
KDG <143414166+gyun97@users.noreply.github.com>
2024-06-21 17:44:28 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 15952 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,107 +1,90 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 class Main {
+
     static int N;
-    static String[][] grid;
+    static char[][] grid;
     static boolean[][] visited;
+    static int count;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-
-
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-
         N = Integer.parseInt(br.readLine());
-        grid = new String[N][N];
-        visited = new boolean[N][N];
-        int count = 0;
-
-        for (int i = 0; i < N; i++) {
-            String[] a = br.readLine().split("");
-            grid[i] = a;
-            Arrays.toString(a);
-        }
-
-
-        String[] a = {"a", "a", "a", "b"};
-
-
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                if (!visited[i][j]) {
-                    count++;
-                    bfs(i, j);
-
-                }
-
-            }
-
-        }
-        sb.append(count + " ");
+        grid = new char[N][N];
         visited = new boolean[N][N];
         count = 0;
 
-
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                if (grid[i][j].equals("G")) grid[i][j] = "R";
+        for (int i = 0; i < N; i++) {
+            String S = br.readLine();
+            for (int j = 0; j < S.length(); j++) {
+                grid[i][j] = S.charAt(j);
             }
-        }
 
+        }
 //        System.out.println(Arrays.deepToString(grid));
 
-
-
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
                     count++;
-                    bfs(i, j);
+                    dfs(i, j);
+                }
 
+
+            }
+
+        }
+
+        sb.append(count + " ");
+        count = 0;
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == 'G') grid[i][j] = 'R';
+
+            }
+
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    count++;
+                    dfs(i, j);
                 }
 
             }
         }
 
         sb.append(count);
+
         System.out.println(sb);
 
 
+            }
 
-
-    }
-
-    private static void bfs(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(x, y));
-        visited[x][y] = true;
-        while (!queue.isEmpty()) {
-            Point current = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
-                if (0 > nx || nx >= N || 0 > ny || ny >= N || visited[nx][ny]) continue;
-                if (grid[nx][ny].equals(grid[current.x][current.y])) {
-                    queue.add(new Point(nx, ny));
-                    visited[nx][ny] = true;
-                }
+    private static void dfs(int x, int y) {
+        char c = grid[x][y];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (0 > nx || nx >= N || 0 > ny || ny >= N || visited[nx][ny]) continue;
+            if (c == grid[nx][ny]) {
+                visited[nx][ny] = true;
+                dfs(nx, ny);
             }
 
         }
+
     }
-
-
 }
+
