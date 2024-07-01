6676730a7aa9983c0cb287271df185af72794af6
KDG <143414166+gyun97@users.noreply.github.com>
2024-07-01 17:53:16 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 class Solution {
-    public boolean solution(String s) {
-        return s.matches("[0-9]{4}|[0-9]{6}");
-        
-    }
+  public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) return s.matches("(^[0-9]*$)");
+        return false;
+  }
 }
