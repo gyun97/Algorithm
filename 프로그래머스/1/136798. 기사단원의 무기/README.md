17cddad6061f08c69e38296477f31942b3fd8ec3
KDG <143414166+gyun97@users.noreply.github.com>
2024-07-25 10:52:33 +0900

    [level 1] Title: 기사단원의 무기, Time: 18.79 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=java) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 144.10 ms
+메모리: 73.6 MB, 시간: 18.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 10:03:32
+2024년 07월 25일 10:52:32
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,41 +1,18 @@
-import java.sql.SQLOutput;
 import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.*;
 
 class Solution {
+
     public int solution(int number, int limit, int power) {
 
-        int[] numbers = IntStream.rangeClosed(1, number).toArray();
-        
-        Map<Integer, Integer> map1 = new HashMap<>();
-        for (int i : numbers) {
-            int half = (int) Math.sqrt(i);
-            map1.put(i, half);
-        }
+        int[] count = new int[number + 1];
 
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-
-        for (Integer i : map1.keySet()) {
-            int count = 0;
-            int j = map1.get(i);
-            for (int k = 1; k <= j; k++) {
-                if (i % k == 0) {
-                    if (Math.pow(k, 2) == i) {
-                        count++;
-                    } else {
-                        count += 2;
-                    }
-                }
-
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            list.add(count);
         }
 
-        return list.stream().map(i -> i > limit ? power : i).mapToInt(i -> i).sum();
+        return Arrays.stream(count).map(i -> i > limit ? power : i).sum();
+        
     }
-
-
 }
