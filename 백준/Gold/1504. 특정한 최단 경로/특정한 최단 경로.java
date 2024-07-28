0af6e38bf4e3cdfc158d958ce21760b884d06404
KDG <143414166+gyun97@users.noreply.github.com>
2024-07-29 01:06:21 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 680 ms, Memory: 68956 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -7,10 +7,11 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Node implements Comparable<Node> {
+
     int end;
     int weight;
 
-    Node(int end, int weight) {
+    public Node(int end, int weight) {
         this.end = end;
         this.weight = weight;
     }
@@ -19,11 +20,9 @@ class Node implements Comparable<Node> {
     public int compareTo(Node o) {
         return weight - o.weight;
     }
-
 }
 
-
-class Main {
+public class Main {
 
     static int N, E;
     static ArrayList<ArrayList<Node>> list;
@@ -31,16 +30,13 @@ class Main {
     static boolean[] visited;
     static final int INF = 200000000;
 
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        E = Integer.parseInt(input[1]);
 
         list = new ArrayList<>();
 
@@ -54,7 +50,7 @@ class Main {
         visited = new boolean[N + 1];
 
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
@@ -63,41 +59,39 @@ class Main {
             list.get(end).add(new Node(start, weight));
         }
 
-        st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int mandatory1 = Integer.parseInt(st.nextToken());
         int mandatory2 = Integer.parseInt(st.nextToken());
 
         int result1 = 0;
-
         result1 += dijkstra(1, mandatory1);
         result1 += dijkstra(mandatory1, mandatory2);
         result1 += dijkstra(mandatory2, N);
+
 
         int result2 = 0;
         result2 += dijkstra(1, mandatory2);
         result2 += dijkstra(mandatory2, mandatory1);
         result2 += dijkstra(mandatory1, N);
 
-        int answer = (result2 >= INF && result1 >= INF) ? -1 : Math.min(result1, result2);
-        sb.append(answer);
+        sb.append((result1 >= INF && result2 >= INF) ? -1 : Math.min(result1, result2));
         System.out.println(sb);
 
     }
 
 
-    public static int dijkstra(int start, int end) {
-
+    private static int dijkstra(int start, int end) {
         Arrays.fill(dist, INF);
         Arrays.fill(visited, false);
 
         PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        pq.offer(new Node(start, 0));
+        pq.add(new Node(start, 0));
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
             Node currentNode = pq.poll();
             int current = currentNode.end;
+
             if (!visited[current]) {
                 visited[current] = true;
 
@@ -106,10 +100,15 @@ class Main {
                         dist[node.end] = dist[current] + node.weight;
                         pq.offer(new Node(node.end, dist[node.end]));
                     }
-
                 }
+
+
             }
+
         }
         return dist[end];
     }
+
+
 }
+
