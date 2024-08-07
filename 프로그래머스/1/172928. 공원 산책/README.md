fe0220da62e30404cfb0c8fb92c2a9ae30111a89
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-07 13:42:19 +0900

    [level 1] Title: 공원 산책, Time: 0.53 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928?language=java) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 0.46 ms
+메모리: 78.4 MB, 시간: 0.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 07일 11:54:47
+2024년 08월 07일 13:42:18
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,84 +1,88 @@
-class Solution {
+import java.util.jar.JarEntry;
 
+class Solution {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
 
-        int x = -1;
-        int y = -1;
-
         int H = park.length; // 세로
-        int W = park[0].length();  // 가로
+        int W = park[0].length(); // 가로
 
-        // Find starting position 'S'
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                if (park[i].charAt(j) == 'S') {
-                    x = i;
-                    y = j;
-                    break;
-                }
+        int x = -1; // 시작 X 좌표
+        int y = -1; // 시작 Y 좌표
+
+
+        for (int i = 0; i < park.length; i++) {
+            if (park[i].contains("S")) {
+                x = i;
+                y = park[i].indexOf('S');
+                break;
             }
-            if (x != -1) break;  // Stop if 'S' is found
         }
 
-        // Process routes
+
         for (String route : routes) {
             String[] array = route.split(" ");
             String dir = array[0];
             int dis = Integer.parseInt(array[1]);
-            int currentX = x;
-            int currentY = y;
+            Integer currentX = x;
+            Integer currentY = y;
             boolean flag = true;
 
             switch (dir) {
-                case "E":
-                    for (int j = 0; j < dis; j++) {
+                case "E" :
+                    for (int i = 0; i < dis; i++) {
                         currentY++;
                         if (currentY >= W || park[currentX].charAt(currentY) == 'X') {
                             flag = false;
                             break;
                         }
                     }
-                    if (flag) y = currentY;  // Update y if move is valid
+                    if (flag) y = currentY;
                     break;
 
-                case "W":
-                    for (int j = 0; j < dis; j++) {
+
+                case "W" :
+                    for (int i = 0; i < dis; i++) {
                         currentY--;
                         if (currentY < 0 || park[currentX].charAt(currentY) == 'X') {
                             flag = false;
                             break;
                         }
                     }
-                    if (flag) y = currentY;  // Update y if move is valid
+                    if (flag) y = currentY;
                     break;
 
-                case "S":
-                    for (int j = 0; j < dis; j++) {
+
+                case "S" :
+                    for (int i = 0; i < dis; i++) {
                         currentX++;
                         if (currentX >= H || park[currentX].charAt(currentY) == 'X') {
                             flag = false;
                             break;
                         }
                     }
-                    if (flag) x = currentX;  // Update x if move is valid
+                    if (flag) x = currentX;
                     break;
 
-                case "N":
-                    for (int j = 0; j < dis; j++) {
+
+                case "N" :
+                    for (int i = 0; i < dis; i++) {
                         currentX--;
                         if (currentX < 0 || park[currentX].charAt(currentY) == 'X') {
                             flag = false;
                             break;
                         }
                     }
-                    if (flag) x = currentX;  // Update x if move is valid
+                    if (flag) x = currentX;
                     break;
-            }
-        }
 
+            }
+
+        }
         answer[0] = x;
         answer[1] = y;
+
         return answer;
+
     }
 }
