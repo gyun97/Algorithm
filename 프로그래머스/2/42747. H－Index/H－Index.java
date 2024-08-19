70b9ce27898d5a9698b710c32d4f10fcbf63c274
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-19 22:58:11 +0900

    [level 2] Title: H-Index, Time: 1.05 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,32 +1,16 @@
-import java.util.Arrays;
+
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        Arrays.sort(citations);
 
-        int max = Arrays.stream(citations).max().getAsInt();
-        int h = Math.min(citations.length, max);
-
-
-        if (h == 0) return h;
-
-        System.out.println(h);
-        LOOP:
-        while (h > 0) {
-            System.out.println(h);
-            int count = 0;
-            for (int citation : citations) {
-                if (citation >= h) {
-                    count++;
-                }
-            }
-            if (count >= h) {
-                break LOOP;
-            }
-            h--;
+        int max = 0;
+        for(int i = citations.length-1; i > -1; i--){
+            int min = (int)Math.min(citations[i], citations.length - i);
+            if(max < min) max = min;
         }
 
-        return h;
-
+        return max;
     }
-
 }
