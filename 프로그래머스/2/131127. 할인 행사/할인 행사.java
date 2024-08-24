90d938eddb2a503f941cc4d7bda335082514684c
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-24 19:29:37 +0900

    [level 2] Title: 할인 행사, Time: 82.44 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,28 +1,35 @@
 import java.util.*;
 
 class Solution {
-        public int solution(String[] want, int[] number, String[] discount) {
-            int answer = 0;
-            int left = 0;
-            int right = 9;
-            Map<String, Integer> map = new HashMap<>();
-            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
-            while(right < discount.length){
-                if(check(map,want,number)) answer++;
-
-                right++;
-                if(right < discount.length)
-                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
-                map.put(discount[left],map.get(discount[left])-1);
-                left++;
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        int days = 10;
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < want.length; i++){
+            map.put(want[i], number[i]);
+        } 
+       
+        for(int i = 0; i < discount.length - days + 1; i++){
+            Map<String, Integer> dMap = new HashMap<>();
+            
+            for(int j = 0; j < days; j++){
+                dMap.put(discount[i + j], dMap.getOrDefault(discount[i + j], 0) + 1);
             }
-
-            return answer;
-        }
-        private boolean check(Map<String,Integer> map,String[] want,int[] number){
-            for(int i =0;i<want.length;i++){
-                if(map.getOrDefault(want[i],0) != number[i]) return false;
+            
+            Boolean isIdentical = true;
+            
+            for(String key : map.keySet()){
+                if(map.get(key) != dMap.get(key)){
+                    isIdentical = false;
+                    break;
+                }
             }
-            return true;
+            
+            answer += isIdentical ? 1 : 0;
         }
+        
+        return answer;
     }
+}
