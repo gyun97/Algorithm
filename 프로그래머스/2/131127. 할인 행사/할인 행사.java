29eb3fff65e43636714f510985703675ffe11a38
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-24 19:28:24 +0900

    [level 2] Title: 할인 행사, Time: 43.12 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,21 +1,28 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
-    	
-        int answer = 0;     
-        
-        for(int i=0; i<discount.length-9; i++) {
-        	int cntday = 0;
-        	for(int j=0; j<want.length; j++) {
-        		int cnt = 0;
-        		for(int k=i; k<i+10; k++) {
-        			if(discount[k].equals(want[j])) cnt++;       			
-        		}
-        		if(cnt<number[j]) break;
-        		else cntday++;
-        	}
-        	if(cntday == want.length) answer++;
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            int left = 0;
+            int right = 9;
+            Map<String, Integer> map = new HashMap<>();
+            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
+            while(right < discount.length){
+                if(check(map,want,number)) answer++;
+
+                right++;
+                if(right < discount.length)
+                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
+                map.put(discount[left],map.get(discount[left])-1);
+                left++;
+            }
+
+            return answer;
         }
-        
-        return answer;
+        private boolean check(Map<String,Integer> map,String[] want,int[] number){
+            for(int i =0;i<want.length;i++){
+                if(map.getOrDefault(want[i],0) != number[i]) return false;
+            }
+            return true;
+        }
     }
-}
