efc5af8bee2f8b2169c2ff0089046a2fda5ccfbe
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-24 18:38:07 +0900

    [level 2] Title: 할인 행사, Time: 111.84 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,9 +4,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
 
+        int answer = 0;
+        
         List<String> wantList = new ArrayList<>();
 
         for (int i = 0; i < want.length; i++) {
@@ -14,17 +17,13 @@ class Solution {
                 wantList.add(want[i]);
             }
         }
-
-        int answer = 0;
-
-        List<String> discountList = Arrays.stream(discount).collect(Collectors.toList());
-        
         Collections.sort(wantList);
+        
+        List<String> discountList = Arrays.stream(discount).collect(Collectors.toList());
         
         for (int i = 0; i <= discount.length - 10; i++) {
             List<String> list = new ArrayList<>(discountList.subList(i, i + 10));
             Collections.sort(list);
-
             if (list.equals(wantList)) {
                 answer++;
             }
