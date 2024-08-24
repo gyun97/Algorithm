5df99412c07d2cc4a955eb43b06558c53f4ecbdf
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-24 18:34:07 +0900

    [level 2] Title: 할인 행사, Time: 92.73 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,26 +1,33 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        ArrayList<String> list = new ArrayList<>();
 
-        for (int i=0; i<want.length; i++) {
-            while (number[i]-- > 0) {
-                list.add(want[i]);
+        List<String> wantList = new ArrayList<>();
+
+        for (int i = 0; i < want.length; i++) {
+            for (int j = 0; j < number[i]; j++) {
+                wantList.add(want[i]);
             }
         }
 
-        for (int i=0; i<discount.length-list.size()+1;i ++) {
-            ArrayList<String> l = (ArrayList<String>) list.clone();
-            for (int j=i; j<i+list.size(); j++) {
-                if (l.contains(discount[j])) {
-                    l.remove(discount[j]);
-                } else {
-                    break;
-                }
+        int answer = 0;
+
+        List<String> discountList = Arrays.stream(discount).collect(Collectors.toList());
+        
+        Collections.sort(wantList);
+        
+        for (int i = 0; i <= discount.length - 10; i++) {
+            List<String> list = new ArrayList<>(discountList.subList(i, i + 10));
+            Collections.sort(list);
+
+            if (list.equals(wantList)) {
+                answer++;
             }
-            answer += l.size()==0 ? 1 : 0;
         }
 
         return answer;
