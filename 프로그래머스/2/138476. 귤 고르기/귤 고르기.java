aa7452d80ba01cfa48667e316d1b592b7a35500e
KDG <143414166+gyun97@users.noreply.github.com>
2024-09-01 01:02:56 +0900

    [level 2] Title: 귤 고르기, Time: 77.21 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,47 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        HashMap<Integer,Integer> map =new HashMap<>();
 
-        Set<Integer> set = new HashSet<>(); // 크기 종류
-
-        for (int i : tangerine) {
-            set.add(i);
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
 
-        Map<Integer, Integer> hm = new HashMap<>();
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2)-map.get(o1));
 
-        for (int i : tangerine) {
-            hm.put(i, hm.getOrDefault(i, 0) + 1);
-        }
-
-        // for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-        //     System.out.println("entry = " + entry);
-        // }
-
-        List<Integer> order = new ArrayList<>(hm.values());
-        Collections.sort(order, Collections.reverseOrder());
-
-        int sum = 0;
-        int count = 1;
-
-        for (int i = 0; i < order.size(); i++) {
-            int j = order.get(i);
-            sum += j;
-            if (sum >= k) {
+        for(Integer key:list){
+            k -=map.get(key);
+            answer++;
+            if(k<=0){
                 break;
-            } else {
-                count++;
             }
         }
-        return count;
 
+        return answer;
     }
-
-//     public static void main(String[] args) {
-//         Solution sol = new Solution();
-//         int k = 6;
-//         int[] tangerine = {1, 3, 2, 5, 4, 5, 2, 3};
-//         sol.solution(k, tangerine);
-
-//     }
 }
