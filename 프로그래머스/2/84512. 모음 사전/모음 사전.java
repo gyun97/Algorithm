a6fb17b189054a417ec38fe7f19c9b3f0883b7d7
KDG <143414166+gyun97@users.noreply.github.com>
2024-08-31 23:23:23 +0900

    [level 2] Title: 모음 사전, Time: 5.49 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,43 @@
+class Solution {
+    static int count = -1;
+    static String[] alphabets = {"A", "E", "I", "O", "U"};
+    static boolean flag = false;
+
+    public int solution(String word) {
+
+        String myWord = "";
+        dfs(word, myWord);
+        System.out.println(count);
+        return count;
+
+    }
+
+    private void dfs(String word, String myWord) {
+
+        if (!flag) {
+            count++;
+        }
+
+        if (myWord.equals(word)) {
+            flag = true;
+            return;
+        }
+
+        if (myWord.length() == 5 || flag) {
+            return;
+        }
+
+        dfs(word, myWord + "A");
+        dfs(word, myWord + "E");
+        dfs(word, myWord + "I");
+        dfs(word, myWord + "O");
+        dfs(word, myWord + "U");
+
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String word = "AAAAE";
+        sol.solution(word);
+    }
+}
