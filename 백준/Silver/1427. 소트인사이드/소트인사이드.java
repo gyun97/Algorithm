26e457c30525426bf4bc90150838ef34b40d3f48
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-01 21:46:18 +0900

    [Silver V] Title: 소트인사이드, Time: 168 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,21 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+
 
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        String N = sc.next();
-        Integer[] arr = new Integer[N.length()];
+        String[] strings = sc.next().split("");
+        int[] nums = new int[strings.length];
 
-        for (int i = 0; i < N.length(); i++){
-            arr[i] = Character.getNumericValue(N.charAt(i));
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(strings[i]);
         }
-        Arrays.sort(arr, Collections.reverseOrder());
 
-        for (int num : arr){
+        for (int i = 0; i < nums.length - 1; i++) {
+            int maxIdx = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[maxIdx] < nums[j]) {
+                    maxIdx = j;
+                }
+            }
+            if (nums[i] < nums[maxIdx]) {
+                int temp = nums[i];
+                nums[i] = nums[maxIdx];
+                nums[maxIdx] = temp;
+            }
+        }
+
+        for (int num : nums) {
             System.out.print(num);
         }
+
     }
 }
