86369a120b3ea38471ec1a47f5ff261d25388c78
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-14 17:26:10 +0900

    [Gold III] Title: 줄 세우기, Time: 436 ms, Memory: 46268 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,25 +1,29 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 학생 수
-        int M = sc.nextInt(); // 키를 비교한 횟수
+        int N = Integer.parseInt(st.nextToken()); // 학생 수
+        int M = Integer.parseInt(st.nextToken()); // 키 비교 횟수
 
-        int[] degree = new int[N + 1];
-        List<ArrayList<Integer>> graph = new ArrayList<>();
+        int[] degree = new int[N + 1]; // 차수 count 배열
+        List<ArrayList<Integer>> graph = new ArrayList<>(); // 인접 리스트(그래프)
 
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
         while (M-- > 0) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             graph.get(A).add(B);
             degree[B]++;
         }
@@ -28,18 +32,21 @@ public class Main {
 
         for (int i = 1; i <= N; i++) {
             if (degree[i] == 0) {
-                queue.add(i);
+                queue.offer(i);
             }
         }
 
         while (!queue.isEmpty()) {
-            int current = queue.poll();
-            System.out.print(current + " ");
-            for (Integer node : graph.get(current)) {
+            int now = queue.poll();
+            sb.append(now).append(" ");
+            for (int node : graph.get(now)) {
                 degree[node]--;
-                if (degree[node] == 0) queue.offer(node);
+                if (degree[node] == 0) {
+                    queue.offer(node);
+                }
             }
         }
+        System.out.println(sb);
 
     }
 }
