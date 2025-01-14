28bce05af06793d52517b83eca94a58ec3c98799
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-14 17:45:19 +0900

    [Gold III] Title: 줄 세우기, Time: 2680 ms, Memory: 39920 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+degree = [0] * (N + 1)
+
+for _ in range(M):
+    A, B = map(int, input().split())
+    graph[A].append(B)
+    degree[B] += 1
+
+# print(graph)
+
+queue = deque()
+
+for i in range(1, N + 1):
+    if degree[i] == 0:
+        queue.append(i)
+
+# print(queue)
+
+while queue:
+    now = queue.popleft()
+    print(now, end=" ")
+    for node in graph[now]:
+        degree[node] -= 1
+        if (degree[node] == 0):
+            queue.append(node)
+
+
+
+
+
+
