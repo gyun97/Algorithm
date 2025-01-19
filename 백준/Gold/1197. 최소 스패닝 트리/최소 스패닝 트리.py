741d6da3450d48095cd635cfe12e936f6ea33c0f
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-19 16:48:08 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 304 ms, Memory: 52740 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -7,43 +7,50 @@ V, E = map(int, input().split())
 parent = [x for x in range(V + 1)]
 pq = []
 
-# 간선 데이터를 최소 힙에 삽입
 for _ in range(E):
     start, end, weight = map(int, input().split())
-    heapq.heappush(pq, (weight, start, end))  # 가중치 기준 정렬
+    heapq.heappush(pq, (weight, start, end))
 
-# 부모 노드를 찾는 함수
+# print(pq)
+
+
 def find(a):
     if parent[a] == a:
         return a
-    parent[a] = find(parent[a])  # 경로 압축
+    parent[a] = find(parent[a])
     return parent[a]
 
-# 같은 집합인지 확인하는 함수
+
 def check_same(a, b):
     a = find(a)
     b = find(b)
-    return a == b
+    if a != b:
+        return False
+    return True
 
-# 두 집합을 합치는 함수
+
 def union(a, b):
     a = find(a)
     b = find(b)
+    # if find(a) != find(b):
     if a != b:
         if a < b:
             parent[b] = a
         else:
             parent[a] = b
 
+
 total_sum = 0
 edge_use = 0
 
-# 크루스칼 알고리즘 실행
+# while pq:
 while edge_use < V - 1 and pq:
-    weight, start, end = heapq.heappop(pq)  # 최소 가중치 간선 꺼내기
-    if not check_same(start, end):  # 사이클이 생기지 않으면
+    weight, start, end = heapq.heappop(pq)
+    if not check_same(start, end):
         total_sum += weight
         union(start, end)
         edge_use += 1
 
 print(total_sum)
+
+
