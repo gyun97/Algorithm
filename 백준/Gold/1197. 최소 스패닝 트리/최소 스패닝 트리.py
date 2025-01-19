addf87b54a0423fc511119fbafee589ead86e6d9
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-19 16:46:16 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 328 ms, Memory: 52740 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,34 +1,31 @@
 import sys
-from queue import PriorityQueue
-sys.setrecursionlimit(10**6)
+import heapq
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-v, e = map(int, input().split())
-parent = [x for x in range(v + 1)]
-# print(parent)
+V, E = map(int, input().split())
+parent = [x for x in range(V + 1)]
+pq = []
 
-pq = PriorityQueue()
-
-for _ in range(e):
+# 간선 데이터를 최소 힙에 삽입
+for _ in range(E):
     start, end, weight = map(int, input().split())
-    pq.put((weight, start, end))  # 가중치 기준이기 때문에 가중치를 가장 앞 순서 배치
+    heapq.heappush(pq, (weight, start, end))  # 가중치 기준 정렬
 
-
+# 부모 노드를 찾는 함수
 def find(a):
     if parent[a] == a:
         return a
-    parent[a] = find(parent[a])
+    parent[a] = find(parent[a])  # 경로 압축
     return parent[a]
 
-
+# 같은 집합인지 확인하는 함수
 def check_same(a, b):
     a = find(a)
     b = find(b)
-    if a == b:
-        return True
-    return False
+    return a == b
 
-
+# 두 집합을 합치는 함수
 def union(a, b):
     a = find(a)
     b = find(b)
@@ -38,16 +35,15 @@ def union(a, b):
         else:
             parent[a] = b
 
-
-use_edge = 0
 total_sum = 0
+edge_use = 0
 
-while use_edge < v - 1:
-    weight, start, end = pq.get()
-    if not check_same(start, end):
+# 크루스칼 알고리즘 실행
+while edge_use < V - 1 and pq:
+    weight, start, end = heapq.heappop(pq)  # 최소 가중치 간선 꺼내기
+    if not check_same(start, end):  # 사이클이 생기지 않으면
         total_sum += weight
-        use_edge += 1
         union(start, end)
-
+        edge_use += 1
 
 print(total_sum)
