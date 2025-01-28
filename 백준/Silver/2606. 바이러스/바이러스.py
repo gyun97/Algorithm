6ebafd970fda133780cce1bd6b3eeca724667b89
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-29 03:21:27 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -17,21 +17,17 @@ visited = [False] * (N + 1)  # 노드 방문 여부 확인 리스트
 count = 0  # 1번 컴퓨터가 감염시키는 연결된 컴퓨터의 수
 
 
-def bfs(x):
-    """bfs로 그래프 탐색해서 1번 노드와 몇 개의 노드가 연결되어 있는지 확인"""
-    global count
-    queue = deque([x])
-    visited[x] = True
-    while queue:
-        cur = queue.popleft()
-        for next in graph[cur]:
-            if not visited[next]:
-                count += 1
-                visited[next] = True
-                queue.append(next)
-
+def dfs(x):
+    """dfs로 그래프 탐색해서 1번 노드와 몇 개의 노드가 연결되어 있는지 확인"""
+    global count  # global로 count 전역 변수로 선언해 함수 내에서도 수정 가능
+    visited[x] = True  # base case(해당 노드의 visited가 True일 시 재귀 탈출)
+    for next in graph[x]:
+        if not visited[next]:
+            count += 1
+            dfs(next)
+            
     return count
 
 
-print(bfs(1))
+print(dfs(1))
 
