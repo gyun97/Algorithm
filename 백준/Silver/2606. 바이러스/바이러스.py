9a05951be1b0c0fdbd042468b14fe6cfabdc11a1
KDG <143414166+gyun97@users.noreply.github.com>
2025-01-29 03:13:28 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,34 +1,37 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
 
-N = int(sys.stdin.readline().strip())
+N = int(input())  # 노드 수
+M = int(input())  # 간선 수
 
-M = int(sys.stdin.readline().strip())
-
+# 노드 연결 정보 담은 그래프 초기화
 graph = [[] for _ in range(N + 1)]
-
-visited = [False] * (N + 1)
-
-count = 0
-
 for _ in range(M):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
+visited = [False] * (N + 1)  # 노드 방문 여부 확인 리스트
+count = 0  # 1번 컴퓨터가 감염시키는 연결된 컴퓨터의 수
+
 
 def bfs(x):
+    """bfs로 그래프 탐색해서 1번 노드와 몇 개의 노드가 연결되어 있는지 확인"""
     global count
     queue = deque([x])
     visited[x] = True
     while queue:
-        x = queue.popleft()
-        for nx in graph[x]:
-            if not visited[nx]:
-                visited[nx] = True
-                queue.append(nx)
+        cur = queue.popleft()
+        for next in graph[cur]:
+            if not visited[next]:
                 count += 1
+                visited[next] = True
+                queue.append(next)
 
-bfs(1)
-print(count)
+    return count
+
+
+print(bfs(1))
+
