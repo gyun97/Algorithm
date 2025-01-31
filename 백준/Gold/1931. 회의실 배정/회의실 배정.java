10fe810cb809b00a77699dd7c4ebdb8d94e1411c
KDG <143414166+gyun97@users.noreply.github.com>
2025-02-01 03:07:25 +0900

    [Gold V] Title: 회의실 배정, Time: 472 ms, Memory: 43308 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -24,13 +23,13 @@ public class Main {
         }
 
 
-        Arrays.sort(meetings, Comparator.comparingInt((int[] a) -> a[1]).thenComparingInt(a -> a[0]));
-//        Arrays.sort(meetings, ((o1, o2) -> {
-//            if (o1[1] == o2[1]) {
-//                return Integer.compare(o1[0], o1[0]);
-//            }
-//            return Integer.compare(o1[1], o2[1]);
-//        } ));
+//        Arrays.sort(meetings, Comparator.comparingInt((int[] a) -> a[1]).thenComparingInt(a -> a[0]));
+        Arrays.sort(meetings, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+            return Integer.compare(o1[1], o2[1]);
+        } );
 
         int curEndTime = 0;
         int count = 0;
