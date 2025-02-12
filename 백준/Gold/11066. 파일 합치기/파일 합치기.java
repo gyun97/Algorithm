e25b9d3760c35d2c0f6019b02377b88bd4ec98fd
KDG <143414166+gyun97@users.noreply.github.com>
2025-02-12 18:55:16 +0900

    [Gold III] Title: 파일 합치기, Time: 468 ms, Memory: 31860 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -13,33 +13,28 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             int K = Integer.parseInt(br.readLine());
-            int[] files = new int[K];
-            int[] sum = new int[K + 1];
 
+            int[] files = new int[K + 1];
+            int[] sum = new int[K + 1];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < K; i++) {
+            for (int i = 1; i <= K; i++) {
                 files[i] = Integer.parseInt(st.nextToken());
-                sum[i + 1] = sum[i] + files[i];
+                sum[i] = sum[i - 1] + files[i];
             }
 
-            int[][] dp = new int[K][K];
-
-            for (int length = 2; length <= K; length++) {
-                for (int start = 0; start <= K - length; start++) {
-                    int end = start + length - 1;
+            int[][] dp = new int[K + 1][K + 1];
+            for (int length = 2; length <= K; length++) { // 합칠 챕터의 길이(개수)
+                for (int start = 1; start <= K - length + 1; start++) { // 합칠 챕터들의 시작 챕터
+                    int end = start + length - 1; // 합칠 챕터들의 마지막 챕터
                     dp[start][end] = Integer.MAX_VALUE;
-
-                    for (int mid = start; mid < end; mid++) {
-                        int cost = dp[start][mid] + dp[mid + 1][end] + (sum[end + 1] - sum[start]);
+                    for (int mid = start; mid < end; mid++) { // 시작 챕터와 마지막 챕터의 중간 챕터
+                        int cost = dp[start][mid] + dp[mid + 1][end] + (sum[end] - sum[start - 1]);
                         dp[start][end] = Math.min(dp[start][end], cost);
                     }
                 }
             }
-//            System.out.println(Arrays.deepToString(dp));
-            sb.append(dp[0][K - 1]).append("\n"); // 정답 저장
+            sb.append(dp[1][K]).append("\n");
         }
-        System.out.print(sb); // 결과 출력
-
-
+        System.out.println(sb);
     }
 }
