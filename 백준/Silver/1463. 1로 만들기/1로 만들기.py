0ab3f927339209585d74a9b1b76a22bffd3a9e5f
KDG <143414166+gyun97@users.noreply.github.com>
2025-03-09 00:40:41 +0900

    [Silver III] Title: 1로 만들기, Time: 476 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,27 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-# 1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
-# 2. X가 2로 나누어 떨어지면, 2로 나눈다.
-# 3. 1을 뺀다.
-
-N = int(input().strip())
-
+N = int(input())
 dp = [0] * (N + 1)
 
 for i in range(2, N + 1):
     dp[i] = dp[i - 1] + 1
-
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i // 3] + 1)
+
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i // 2] + 1)
 
-
 print(dp[N])
-
-
-
-
-
-
