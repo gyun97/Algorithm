d261adf098bca299d71dacda2bf6019a5d403378
KDG <143414166+gyun97@users.noreply.github.com>
2025-03-09 00:35:56 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18132 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,31 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-
         int[] dp = new int[N + 1];
 
-        for (int i = 2; i < N + 1; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 3 == 0) {
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1; // - 1하는 경우
+
+            if (i % 3 == 0) { // 3 나누는 경우
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
-            if (i % 2 == 0) {
+
+            if (i % 2 == 0) { // 2 나누는 경우
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
+
         }
+
         sb.append(dp[N]);
         System.out.println(sb);
-
-
     }
-
-
-
 }
+
+
+
+
+
+
