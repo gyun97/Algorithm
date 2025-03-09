9702b2f403eb95855d0860ee3ead6d2cd464422f
KDG <143414166+gyun97@users.noreply.github.com>
2025-03-09 18:24:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 120 ms, Memory: 15980 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -2,24 +2,30 @@ import java.io.*;
 
 public class Main {
 
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
+        dp = new int[N + 1];
         dp[1] = 1;
         dp[0] = 1;
-
-        for (int i = 2; i < N + 1; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-        }
+        recur(N);
 
 //        System.out.println(Arrays.toString(dp));
 
         bw.write(dp[N] + "\n");
         bw.flush();
         bw.close();
+    }
+
+    public static int recur(int n) {
+        if(dp[n] != 0) return dp[n];
+        dp[n] = (recur(n - 1) + recur(n - 2)) % 10007;
+        return dp[n];
+
     }
 }
 
