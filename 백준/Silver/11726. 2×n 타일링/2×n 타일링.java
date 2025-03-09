db4ead9e1a29c516f3a376712f8aa46a9adb1b70
KDG <143414166+gyun97@users.noreply.github.com>
2025-03-09 18:00:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 16064 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,27 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[1000 + 1];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
         dp[1] = 1;
-        dp[2] = 2;
+        dp[0] = 1;
 
-        for (int i = 3; i < n + 1; i++) {
+        for (int i = 2; i < N + 1; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
-        sb.append(dp[n]);
-        System.out.println(sb);
+//        System.out.println(Arrays.toString(dp));
 
-
+        bw.write(dp[N] + "\n");
+        bw.flush();
+        bw.close();
     }
-
 }
+
+
+
+
+
+
