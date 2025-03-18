9ed22dcdf06c4128a00c13190886438f02a877b5
KDG <143414166+gyun97@users.noreply.github.com>
2025-03-19 00:54:20 +0900

    [Gold IV] Title: LCS 2, Time: 444 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,34 +1,45 @@
 import sys
+
 input = sys.stdin.readline
 
 str1 = input().strip()
 str2 = input().strip()
 
-# ✅ dp 배열 올바르게 초기화
-dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
+N = len(str1)
+M = len(str2)
 
-# ✅ DP 테이블 채우기
-for i in range(1, len(str1) + 1):
-    for j in range(1, len(str2) + 1):
-        if str1[i - 1] == str2[j - 1]:
+# dp 배열 크기 수정 (M+1) x (N+1)
+dp = [[0] * (N + 1) for _ in range(M + 1)]
+
+# dp 테이블 채우기
+for i in range(1, M + 1):  # M을 사용
+    for j in range(1, N + 1):  # N을 사용
+        if str1[j - 1] == str2[i - 1]:  # 인덱스 수정
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-# ✅ LCS 추적
-i, j = len(str1), len(str2)
-answer = []
+# LCS 길이 출력
+answer = dp[M][N]  # 올바른 위치에서 LCS 길이 가져오기
+print(answer)
 
-while i > 0 and j > 0:
-    if str1[i - 1] == str2[j - 1]:
-        answer.append(str1[i - 1])
+# LCS 문자열 복원
+if answer == 0:  # 정수값이므로 len() 대신 비교 연산
+    sys.exit(0)
+
+i = M
+j = N
+arr = []
+
+while i > 0 and j > 0:  # IndexError 방지
+    if str1[j - 1] == str2[i - 1]:  # 인덱스 수정
+        arr.append(str1[j - 1])
         i -= 1
         j -= 1
-    elif i > 0 and dp[i - 1][j] >= dp[i][j - 1]:  # ✅ i가 0보다 클 때만 검사
+    elif dp[i - 1][j] > dp[i][j - 1]:
         i -= 1
-    elif j > 0:  # ✅ j가 0보다 클 때만 검사
+    else:
         j -= 1
 
-# ✅ 결과 출력
-print(dp[len(str1)][len(str2)])
-print("".join(answer[::-1]))  # 🔹 LCS는 뒤집어서 출력해야 함!
+# LCS 문자열 출력 (뒤집어서 출력)
+print("".join(arr[::-1]))
