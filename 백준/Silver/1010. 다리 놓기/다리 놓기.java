8024bae10e051c608b34b75c288132f49e36b8ce
KDG <143414166+gyun97@users.noreply.github.com>
2025-04-20 22:10:09 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[][] dp = new int[30][30];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,24 +14,31 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // 서쪽
-            int M = Integer.parseInt(st.nextToken()); // 동쪽
+            int N = Integer.parseInt(st.nextToken()); // 서
+            int M = Integer.parseInt(st.nextToken()); // 동
 
-            sb.append(combination(M, N)).append("\n");
+            sb.append(comb(M, N)).append("\n");
+
         }
-
         System.out.println(sb);
+
     }
 
-    public static BigInteger combination(int n, int r) {
-        return factorial(n).divide(factorial(r).multiply(factorial(n - r)));
-    }
-
-    public static BigInteger factorial(int n) {
-        BigInteger result = BigInteger.ONE;
-        for (int i = 2; i <= n; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
+    private static int comb(int n, int r) {
+        if (dp[n][r] != 0) {
+            return dp[n][r];
         }
-        return result;
+
+        if (n == r || r == 0) {
+            return 1;
+        }
+
+        return dp[n][r] = comb(n - 1, r - 1) + comb(n - 1, r);
+
     }
+
+
 }
+
+
+
