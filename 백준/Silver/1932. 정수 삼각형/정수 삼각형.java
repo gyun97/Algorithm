9d0c0c4d5071035d4e0fb44076aae7a2d81be214
KDG <143414166+gyun97@users.noreply.github.com>
2025-05-03 16:58:01 +0900

    [Silver I] Title: 정수 삼각형, Time: 284 ms, Memory: 27560 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,36 +9,41 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        List<Integer>[] triangles = new ArrayList[N];
-        List<Integer>[] dp = new ArrayList[N];
+        int[][] triangles = new int[N][];
+        int[][] dp = new int[N][];
 
         for (int i = 0; i < N; i++) {
-            triangles[i] = new ArrayList<>();
-            dp[i] = new ArrayList<>();
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            while (st.hasMoreTokens()) {
-                triangles[i].add(Integer.parseInt(st.nextToken()));
-                dp[i].add(0);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            triangles[i] = new int[i + 1];
+            dp[i] = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                triangles[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = 0;
             }
         }
 
+        // Bottom-Up 방식을 위해 맨 아래 dp는 해당 칸의 삼각형 값으로 초기화
+        // Top-Down 방식을 사용하면 같은 노드를 여러 번 방문해야 하고 메모제이션을 통해 중간중간 결과 저장과정 필요
         for (int i = 0; i < N; i++) {
-            dp[N - 1].set(i, triangles[N - 1].get(i));
+            dp[N - 1][i] = triangles[N - 1][i];
         }
 
-//        System.out.println(Arrays.toString(triangles));
+//        System.out.println(Arrays.deepToString(triangles));
+//        System.out.println(Arrays.deepToString(dp));
 
         for (int i = N - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[i].set(j, Math.max(dp[i + 1].get(j), dp[i + 1].get(j + 1)) + triangles[i].get(j));
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangles[i][j];
+
             }
         }
 
-//        System.out.println(Arrays.toString(dp));
-        bw.write(dp[0].get(0) + "\n");
+        bw.write(dp[0][0] + "\n");
         bw.flush();
         bw.close();
+        
 
+        
 
     }
 }
