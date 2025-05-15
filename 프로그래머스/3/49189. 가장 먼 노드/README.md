6c37eba86b752e4374e1f69731edb55ce30c28e1
KDG <143414166+gyun97@users.noreply.github.com>
2025-05-15 18:26:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 123.30 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 30.77 ms
+메모리: 123 MB, 시간: 123.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 15일 18:08:49
+2025년 05월 15일 18:26:33
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,67 @@
 import java.util.*;
 
 class Solution {
+    
+    static boolean[] visited;
+    static List<List<Integer>> graph;
+    static int[] dist;
+    
     public int solution(int n, int[][] edge) {
         
-        boolean[] visited = new boolean[n + 1];
-        List<Integer>[] graph = new ArrayList[n + 1];
-        int[] dist = new int[n + 1];
-            
+        graph = new ArrayList<>();
+        visited = new boolean[n + 1];
+        
         for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            graph.add(new ArrayList<>());
         }
         
         for (int i = 0; i < edge.length; i++) {
-            graph[edge[i][0]].add(edge[i][1]);
-            graph[edge[i][1]].add(edge[i][0]);
+            int a = edge[i][0];
+            int b = edge[i][1];
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
         
-        Queue<Integer> q = new ArrayDeque<>();
-        visited[1] = true;
-        q.add(1);
+        dist = new int[n + 1];
+    
+        int max = bfs(1);
         
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int next : graph[cur]) {
-                if (!visited[next]) {
-                    dist[next] = dist[cur] + 1;
-                    visited[next] = true;
-                    q.offer(next);
-                }
-            }
-        }
+        // System.out.println(Arrays.toString(dist));
+        // graph.stream().forEach(i -> System.out.println(i));
         
-        int max = Arrays.stream(dist).max().getAsInt();
         int answer = 0;
+        
         for (int i : dist) {
             if (i == max) answer++;
         }
         
-        // System.out.println(Arrays.deepToString(graph));
-        // System.out.println(Arrays.toString(dist));
-    
         return answer;
+            
     }
     
-    
+    public int bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        visited[start] = true;
+        q.offer(start);
+        
+        int max = 0;
+        
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int next : graph.get(cur)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    dist[next] = dist[cur] + 1;
+                    q.offer(next);
+                    
+                    if (dist[next] > 0) {
+                        max = dist[next];
+                    }
+                    
+                }
+            }
+        }
+        return max;
+    }
     
 }
