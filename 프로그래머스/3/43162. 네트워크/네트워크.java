ca197f73d9968a731ace70e8a9473370f5304467
KDG <143414166+gyun97@users.noreply.github.com>
2025-05-16 01:48:21 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,75 +1,29 @@
 import java.util.*;
 
 class Solution {
-    
-    static boolean[] visited;
-    static List<Integer>[] graph;
-    static int answer;
-    
     public int solution(int n, int[][] computers) {
         
-        graph = new ArrayList[n + 1];
+        boolean[] visited = new boolean[n];
         
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    graph[i + 1].add(j + 1);
-                }
+            if (!visited[i]) {
+                dfs(i, visited, computers);
+                answer++;
             }
         }
-        
-        System.out.println(Arrays.deepToString(graph));
-        
-        answer = 0;
-        
-        visited = new boolean[n + 1];
-        
-        for (int i = 1; i <= n; i++) {
-            bfs(i);
-        }
-        
     
-        
-//         visited = new boolean[n + 1][n + 1];
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 if (!visited)
-//             }
-//         }
-        
         return answer;
     }
     
-    
-    public void bfs(int start) {
-        
-        if (visited[start]) return;
-        
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        visited[start] = true;
-        
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int next : graph[cur]) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    q.offer(next);
-                }
+    public void dfs(int computer, boolean[] visited, int[][] computers) {
+        visited[computer] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (!visited[i] && computers[computer][i] == 1) {
+                dfs(i, visited, computers);
             }
         }
-        
-        answer++;
-        
-        
-        
-        
-        
-        
         
     }
 }
