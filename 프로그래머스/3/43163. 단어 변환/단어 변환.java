8822afa6f1b1ce77c1dbf5085c611aad609aea11
KDG <143414166+gyun97@users.noreply.github.com>
2025-05-16 19:57:59 +0900

    [level 3] Title: 단어 변환, Time: 0.87 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,30 +1,50 @@
+import java.util.*;
+
+class Node {
+    String word;
+    int depth;
+    
+    public Node(String word, int depth) {
+        this.word = word;
+        this.depth = depth;
+    }
+    
+}
+
 class Solution {
     
-    static boolean[] visited;
-    static int answer = Integer.MAX_VALUE;
+    boolean[] visited;
     
     public int solution(String begin, String target, String[] words) {
         
         visited = new boolean[words.length];
-        dfs(begin, target, words, 0);
         
-        return answer == Integer.MAX_VALUE ? 0 : answer;
+        int answer = bfs(begin, target, words, 0);
+        return answer;
     }
     
-    public void dfs(String cur, String target, String[] words, int depth) {
-        if (cur.equals(target)) {
-            answer = Math.min(answer, depth);
-            return;
-        }
+    public int bfs(String begin, String target, String[] words, int depth) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(begin, 0));
         
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (!visited[i] && canChange(cur, word)) {
-                visited[i] = true;
-                dfs(word, target, words, depth + 1);
-                visited[i] = false;
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            String curWord = node.word;
+            int curDepth = node.depth;
+            
+            if (curWord.equals(target)) return curDepth;
+            
+            for (int i = 0; i < words.length; i++) {
+                String word = words[i];
+                
+                if (!visited[i] && canChange(curWord, word)) {
+                    visited[i] = true;
+                    q.offer(new Node(word, curDepth + 1));
+                }
+                
             }
         }
+        return 0;
         
     }
     
@@ -33,9 +53,6 @@ class Solution {
         for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) != b.charAt(i)) diff++;
         }
-        
         return diff == 1;
-        
     }
-    
-}
+} 
