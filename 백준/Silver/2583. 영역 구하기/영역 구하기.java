1d11e943b4f35f9054567a0bf776ce4f9e3962be
KDG <143414166+gyun97@users.noreply.github.com>
2025-06-11 17:28:08 +0900

    [Silver I] Title: 영역 구하기, Time: 104 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,16 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int M, N, K;
-    static int[][] area;
-    static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int[][] board;
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +18,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken()); // 열
         K = Integer.parseInt(st.nextToken()); // 내부 직사각형 개수
 
-        area = new int[M][N];
+        board = new int[M][N];
 
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
@@ -29,51 +26,57 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            areaSplit(x1, y1, x2, y2);
+            boardSplit(x1, y1, x2, y2);
         }
 
-        visited = new boolean[M][N];
-        ArrayList<Integer> results = new ArrayList<>();
+        List<Integer> results = new ArrayList<>();
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && area[i][j] == 0) {
-                    int result = dfs(i, j);
-                    results.add(result);
-
+                if (board[i][j] == 0) {
+                    int reuslt = dfs(i, j);
+                    results.add(reuslt);
                 }
+
             }
         }
+
         Collections.sort(results);
 
-        System.out.println(results.size());
-        results.forEach(i -> System.out.print(i + " "));
+        sb.append(results.size()).append("\n");
+
+        for (Integer result : results) {
+            sb.append(result).append(" ");
+        }
+
+        System.out.println(sb);
     }
 
     private static int dfs(int x, int y) {
-        visited[x][y] = true;
+        board[x][y] = 1;
         int size = 1;
+
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (0 <= nx && nx < M && 0 <= ny && ny < N) {
-                if (!visited[nx][ny] && area[nx][ny] == 0) {
-                    size += dfs(nx, ny);
-                }
+            if (0 > nx || nx >= M || 0 > ny || ny >= N) {
+                continue;
+            }
+
+            if (board[nx][ny] == 0) {
+                size += dfs(nx, ny);
             }
         }
         return size;
-
     }
 
-    private static void areaSplit(int x1, int y1, int x2, int y2) {
+    private static void boardSplit(int x1, int y1, int x2, int y2) {
         for (int y = y1; y < y2; y++) {
             for (int x = x1; x < x2; x++) {
-                area[y][x] = 1;
+                board[y][x] = 1;
             }
+
         }
-
-
     }
 }
 
