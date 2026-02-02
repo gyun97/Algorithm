42268a98c1e7d48dd3c3a36559d2926686de5ed2
KDG <143414166+gyun97@users.noreply.github.com>
2026-02-02 21:59:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.18 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 32.27 ms
+메모리: 111 MB, 시간: 45.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 08일 19:02:11
+2026년 02월 02일 21:59:30
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int[] arr) {
-
-        LinkedList<Integer> queue = new LinkedList<>();
-
-        queue.add(arr[0]);
-
+    public int[] solution(int []arr) {
+        
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(arr[0]);
+        
         for (int i = 1; i < arr.length; i++) {
-            if (queue.getLast() != arr[i]) {
-                queue.offer(arr[i]);
-            }
+            if (arr[i] != q.peekLast()) q.add(arr[i]);
+        }
+        
+        // System.out.println(q);
+    
+        
+        int[] answer = new int[q.size()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = q.poll();
+            // System.out.println(answer[i]);
         }
 
-
-        int[] answer = queue.stream().mapToInt(Integer::intValue).toArray();
-        
         return answer;
-
-
     }
 }
