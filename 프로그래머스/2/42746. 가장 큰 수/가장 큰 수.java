bd071f766f560ac2b2e69dbca6cd04f91aafc4eb
KDG <143414166+gyun97@users.noreply.github.com>
2026-02-07 19:09:08 +0900

    [level 2] Title: 가장 큰 수, Time: 176.71 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < numbers.length; i++) {
-            list.add(numbers[i]);
+        String[] arr = new String[numbers.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
         }
-        Collections.sort(list, (a, b) -> {
-            String as = String.valueOf(a), bs = String.valueOf(b);
-            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
-        });
+        
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (arr[0].equals("0")) return "0";
+        
         StringBuilder sb = new StringBuilder();
-        for(Integer i : list) {
-            sb.append(i);
-        }
-        answer = sb.toString();
-        if(answer.charAt(0) == '0') {
-            return "0";
-        }else {
-            return answer;
-        }
+        for (String s : arr) sb.append(s);
+        
+        return sb.toString();
+        
+        
+        
+        
     }
 }
