b4dffb44be92b0d072e1ca4a16f234eda0db0b96
KDG <143414166+gyun97@users.noreply.github.com>
2026-02-13 03:08:36 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-
-        int[] answer = new int[2];
-
-        double root = Math.sqrt(yellow);
-        int a = 0;
-        int b = 0;
-
-        for (int i = 1; i <= root; i++) {
-            if (yellow % i == 0) {
-                a = i;
-                b = yellow / i;
-            }
-            if ((a + b) * 2 + 4 == brown) {
-                answer[0] = Math.max(a, b) + 2;
-                answer[1] = Math.min(a, b) + 2;
-            }
-
+        
+        int width = 0;
+        int height = 0;
+        
+        for (int i = 1; i <= yellow; i++) {
+            if (yellow % i != 0) continue;
+            
+            width = i; // 가로
+            height = yellow / i; // 세로
+            
+            if (width < height) continue;
+            
+            int brownTemp = ((width + 2) * 2) + (height * 2);
+            if (brown == brownTemp) break;
+            
         }
         
-        return answer;
+        return new int[]{width + 2, height + 2};
+        
+        
     }
 }
-
-
